8d01b574489afaf8a9f45b481e5b08fdd132b1e2
lucas nunes <lucasnunes9413@gmail.com>
2024-12-27 14:31:31 -0300

    add form section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TagSingleComponent } from '../components/tag-single/tag-single.component';
 import { ProjectCardComponent } from '../components/project-card/project-card.component';
+import { FormContactComponent } from '../components/form-contact/form-contact.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TagSingleComponent, ProjectCardComponent],
+  imports: [TagSingleComponent, ProjectCardComponent, FormContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
