4a9ee0f31cc705d77356a9a1af303553f7de99ee
lucas nunes <lucasnunes9413@gmail.com>
2024-12-27 14:52:52 -0300

    contact form add

--- src/app/components/form-contact/form-contact.component.ts
+++ src/app/components/form-contact/form-contact.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-contact',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './form-contact.component.html',
   styleUrl: './form-contact.component.css'
 })
 export class FormContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(/^(\(?\d{2}\)?\s?)?\d{4,5}-?\d{4}$/)]],
+      company: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      const form = document.getElementById('contactForm') as HTMLFormElement;
+      form.submit();
+    } else {
+      console.error('Formulário inválido');
+    }
+  }
 }
